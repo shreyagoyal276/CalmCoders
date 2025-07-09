d970f08633fa7076f0b3542957290c73ded178db
Shreya Goyal <shreyagoyal401@gmail.com>
2025-07-09 23:00:29 +0530

    welcome page issue fixed

--- website/app.py
+++ website/app.py
@@ -11,8 +11,13 @@ with open('tfidf_vectorizer.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
 
 @app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/index')
 def index():
     return render_template('index.html')
+
 
 @app.route('/relax', methods=['POST'])
 def relax():
